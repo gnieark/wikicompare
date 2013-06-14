d2cf73725b0622f02e8f81e8be3ccfda6dc9f8c5
Yannick Buron <yannick@gmail.com>
2013-06-14 21:08:16 +0200

    needs ready 13/24

--- js/wikicompare_comparative_table.js
+++ js/wikicompare_comparative_table.js
@@ -963,7 +963,7 @@ Drupal.behaviors.WikicompareComparativeTable = {
 
 
 /*
-11/24
+13/24
 21 fichiers
 faire les TODOS
 Faire le INSTALL.txt, installer une plateforme de demo et une de stresstest sur le serveur.
